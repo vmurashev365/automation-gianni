61b920e7faa3a9f00a42e8d6d8023c38cf7d7581
Victor Murashev <vmurashev@gmail.com>
2022-09-28 13:21:00 +0300

    contact-us-runners-reporting

--- src/test/java/runners/mainRunner.java
+++ src/test/java/runners/mainRunner.java
@@ -5,6 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
-        tags = "@test123", monochrome = true, dryRun = false)
+        tags = "@contact-us", monochrome = true, dryRun = false,
+        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucmber.json"})
 public class mainRunner extends AbstractTestNGCucumberTests {
 }
