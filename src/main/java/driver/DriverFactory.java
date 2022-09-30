b84764f86a4584340835997fdf67ec42dbd8704c
Victor Murashev <vmurashev@gmail.com>
2022-09-30 11:24:26 +0300

    driver_factory_properties_file_part-3
    involving config.properties functionality

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -7,24 +7,31 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class DriverFactory {
     private static ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
 
-    public static WebDriver getDriver() {
+    public static WebDriver getDriver()  {
         if (webDriver.get() == null) {
-            webDriver.set(createDriver());
+            try {
+                webDriver.set(createDriver());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
         return webDriver.get();
     }
 
-    private static WebDriver createDriver() {
+
+
+    private static WebDriver createDriver() throws IOException {
         WebDriver driver = null;
 
-        String browserType = "chrome";
-
-        switch (browserType) {
+        switch (getBrowserType()) {
             case "chrome" -> {
-
                 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/driver/drivers/chromedriver.exe");
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
@@ -41,6 +48,20 @@ public class DriverFactory {
         }
         driver.manage().window().maximize();
         return driver;
+    }
+
+    private static String getBrowserType() {
+        String browserType = null;
+        try {
+            Properties properties = new Properties();
+            FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/properties/config.properties");
+            properties.load(file);
+            browserType = properties.getProperty("browser").toLowerCase().trim();
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return browserType;
     }
 
     public static void cleanupDriver() {
