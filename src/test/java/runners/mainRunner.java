50210c6049f5cde6bd5b580f52f3dd3d811c82c2
Victor Murashev <vmurashev@gmail.com>
2022-09-30 11:44:42 +0300

    parallel-test-execution

--- src/test/java/runners/mainRunner.java
+++ src/test/java/runners/mainRunner.java
@@ -3,9 +3,15 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
         tags = "@regression", monochrome = true, dryRun = false,
         plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
 public class mainRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
