e12399c1beb4130d1e3543ec0920e87dccc57cb4
Victor Murashev <vmurashev@gmail.com>
2022-11-20 18:27:36 +0200

    pandashop-order-preparing

--- src/main/java/pageObjects/PandashopOrder_PO.java
+++ src/main/java/pageObjects/PandashopOrder_PO.java
@@ -6,13 +6,13 @@ import utils.Global_Vars;
 
 public class PandashopOrder_PO extends Base_PO {
 
-    private @FindBy (xpath = "//div[@class='profileIco ico d-none d-sm-inline']//a") WebElement accountEntry ;
-    private @FindBy (xpath = "//input[@id='txtEmail']") WebElement accountUserName;
-    private @FindBy (xpath = "//input[@id='txtPassword']") WebElement accountPassword;
-    private @FindBy( xpath = "//input[@class='searchPnl-input ui-autocomplete-input']") WebElement searchProduct_TextField;
+    private @FindBy(xpath = "//div[@class='profileIco ico d-none d-sm-inline']//a") WebElement accountEntry;
+    private @FindBy(xpath = "//input[@id='txtEmail']") WebElement accountUserName;
+    private @FindBy(xpath = "//input[@id='txtPassword']") WebElement accountPassword;
+    private @FindBy(xpath = "//input[@class='searchPnl-input ui-autocomplete-input']") WebElement searchProduct_TextField;
     private @FindBy(xpath = "(//i[@class='fa fa-search'])[1]") WebElement searchProduct_Button;
     private @FindBy(xpath = "//span[@class='oneProd-inCos-inner btn btn-green']") WebElement buy_Button;
-    private @FindBy( xpath = "//a[@class='btn btn-green w-100']") WebElement cart_Button;
+    private @FindBy(xpath = "//a[@class='btn btn-green w-100']") WebElement cart_Button;
     private @FindBy(css = ".btn.btn.btn-green.text-white.w-100.w-md-auto.btn-gotoSubmitOrder") WebElement prepare_buy_Button;
     //private @FindBy(xpath = "//input[@class='btn btn-green text-white w-100 w-md-auto btn-gotoSubmitOrder']") WebElement prepare_buy_Button;
     private @FindBy(xpath = "(//div[@class='col-12 mt-15px'])[1]") WebElement enter_Button;
@@ -36,29 +36,45 @@ public class PandashopOrder_PO extends Base_PO {
     }
 
 
-    public void clickOnEntryButton() {accountEntry.click();}
-    public void clickAccountUserName(){ accountUserName.click();}
+    public void clickOnEntryButton() {
+        waitElementToBeClickable(accountEntry);
+    }
+
     public void setAccountUserName(String username) {
-        sendKeys(accountUserName, username);}
-    public void setAccountPassword(String password) {sendKeys(accountPassword, password);}
+        sendKeys(accountUserName, username);
+    }
+
+    public void setAccountPassword(String password) {
+        sendKeys(accountPassword, password);
+    }
+
     public void navigate_toPandashopHome() {
         navigate_To_URL(Global_Vars.PANDA_URL);
     }
+
     public void setProductName(String product) {
         sendKeys(searchProduct_TextField, product);
     }
-    public void clickOnSearchButton() { searchProduct_Button.click(); }
-    public void clickOnBuyButton() { buy_Button.click(); }
-    public void clickOnCartButton() {
-        cart_Button.click();
+
+    public void clickOnSearchButton() {
+        waitElementToBeClickable(searchProduct_Button);
     }
-    public void clickOnPrepareBuyButton() { prepare_buy_Button.click();}
+
+    public void clickOnBuyButton() {
+        waitElementToBeClickable(buy_Button);
+    }
+
+    public void clickOnCartButton() {
+        waitElementToBeClickable(cart_Button);
+    }
+
+    public void clickOnPrepareBuyButton() {
+        waitElementToBeClickable(prepare_buy_Button);
+    }
 
     public void clickOnEnterButton() {
-        enter_Button.click(); }
-
-
-
+        waitElementToBeClickable(enter_Button);
+    }
 
 
 }
